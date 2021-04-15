c6941861e61c376377da9feb3607cfbb85c90325
Dorkside <jdmartin@gmx.fr>
2021-04-15 19:51:55 +0200

    feat: install firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 
-void main() {
+Future main() async {
+  // NOTE: The filename will default to .env and doesn't need to be defined in this case
+  await DotEnv.load(fileName: ".env");
+  //...runapp
   runApp(TodoApp());
 }
 
